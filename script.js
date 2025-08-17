b7478da6b3677881f0552f1904e3984e5c488de3
Samyamay Bhattacharjee <samyamay2005@gmail.com>
2025-08-17 13:26:13 +0530

    basic functions changed

--- script.js
+++
@@ -1,7 +0,0 @@
-/*Hamburger toggle on media width @768p*/
-const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.navLinks');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
